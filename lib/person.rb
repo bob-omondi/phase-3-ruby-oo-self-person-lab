c50b4798ed1e1de768b2ea8f63f8d1cf6fe9d46a
+ <+>
2022-11-04 03:30:29 +0300

    completed

--- lib/person.rb
+++ lib/person.rb
@@ -3,36 +3,19 @@ class Person
   attr_accessor :bank_account
   attr_reader :name, :happiness, :hygiene
 
-  def initialize(name)
+  def initialize(name, bank_account = 25, happiness=8, hygiene = 8)
     @name = name
-    @bank_account = 25
-    @happiness = 8
-    @hygiene = 8
-  end
+    @bank_account = bank_account
+    @happiness= happiness
+    @hygiene=hygiene
+end
+def happiness=(happiness) 
+  @happiness = happiness.clamp(0,10)
+ end
 
-  def happiness=(num)
-    @happiness =  if num > 10
-                    10
-                  elsif num < 0
-                    0
-                  else
-                    num
-                  end
-
-    
-  end
-
-  def hygiene=(num)
-    @hygiene =  if num > 10
-                  10
-                elsif num < 0
-                  0
-                else
-                  num
-                end
-
-
-  end
+ def hygiene=(hygiene) 
+  @hygiene = hygiene.clamp(0,10)
+ end
 
   def clean?
     self.hygiene > 7
